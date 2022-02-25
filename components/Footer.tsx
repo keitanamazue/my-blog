c86d4d5cb0ce3e21fb74aa3993601197f6943f7c
keita namazue <keitanamazue@gmail.com>
2022-02-26 00:54:45 +0900

    🚀 first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="bg-black py-6">
+      <p className="text-center text-xs text-white">
+        copy right ©️ ALL RIGHT RESERVED BY keita namazue
+      </p>
+    </footer>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full bg-gray-300">
+      <ul className="flex items-center justify-center space-x-6  px-4 py-4 text-base md:space-x-10 md:border-b-2  lg:space-x-20">
+        <li className="">
+          <Link href="/blockchain">Blockchain</Link>
+        </li>
+        <li>
+          <Link href="/Bitcoin">Bitcoin</Link>
+        </li>
+        <li>
+          <Link href="/Ethreum">Ethreum</Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
