031b6fe00c7cfde7aa32bf3fa82f7058a4bd7d4f
keita namazue <keitanamazue@gmail.com>
2022-03-03 16:45:05 +0900

    🐛

--- components/SliderPost.tsx
+++ components/SliderPost.tsx
@@ -1,19 +1,19 @@
-type blogProps = {
-  title: string
-  category: string | []
-  publishedAt: string
-  eyecatch: string
-  body: string
-  id: string
-}
+// type blogProps = {
+//   title: string
+//   category: string | []
+//   publishedAt: string
+//   eyecatch: string
+//   body: string
+//   id: string
+// }
 
-export const SliderPost = (props: blogProps) => {
+// export const SliderPost = (props: blogProps) => {
   
-  return (
-    <div className="mx-auto flex max-w-6xl overflow-hidden pt-14 lg:pt-20">
-        <div className="">
-          <img src={props?.eyecatch?.url} alt="" />
-        </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="mx-auto flex max-w-6xl overflow-hidden pt-14 lg:pt-20">
+//         <div className="">
+//           <img src={props?.eyecatch?.url} alt="" />
+//         </div>
+//     </div>
+//   )
+// }
